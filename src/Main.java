c27e24a2b3234f3b75658911dbf0303f1aeab031
Kim Zugic <kim.zugic@live.com>
2024-10-01 14:35:10 +0200

    Added a while loop

--- src/Main.java
+++ src/Main.java
@@ -30,13 +30,34 @@ public class Main {
         }
 
         boolean isRunning = true;
+
         while (isRunning) {
-            System.out.println("Choose which plant to water: ");
+
+            String userInput;
+
+            System.out.println("\nChoose which plant to water: ");
+            String choice = input.nextLine().toLowerCase();
+
+
+            for (Plants plant : allPlants) {
+                if (choice.equals(plant.getName().toLowerCase())) {
+                    plant.printLiquidInfo();
+                    break;
+                }
+            }
+
+            System.out.println("\nDo you want to water another plant? y/n");
+            System.out.println("(Type \"in store\" to see available plants)");
+            userInput = input.nextLine().toLowerCase();
+
+            if (userInput.equals("n")) {
+                isRunning = false;
+                System.out.println("Exiting program...");
+            } else if (userInput.equals("in store")) {
+                seePlantsInStore(allPlants);
+            }
         }
     }
-
-
-
     public static void main(String[] args) {
         Main sprint1 = new Main();
     }
